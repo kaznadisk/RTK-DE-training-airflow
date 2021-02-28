51dd848a8665fa9ffe3c47dcb561e047c32b2925
Sergey Kaznadi <47914957+kaznadisk@users.noreply.github.com>
2021-03-01 01:42:06 +0300

    Update spacex_dag.py

--- spacex_dag.py
+++ spacex_dag.py
@@ -6,7 +6,7 @@ from datetime import datetime, timedelta
 default_args = {
     "owner": "airflow",
     "depends_on_past": False,
-    "start_date": datetime(2018, 1, 1),
+    "start_date": datetime(2005, 1, 1),
     "email": ["airflow@airflow.com"],
     "email_on_failure": False,
     "email_on_retry": False,
@@ -16,17 +16,19 @@ default_args = {
 
 dag = DAG("spacex", default_args=default_args, schedule_interval="0 0 1 1 *")
 
-t1 = BashOperator(
-    task_id="get_data", 
-    bash_command="python3 /root/airflow/dags/spacex/load_launches.py -y {{ execution_date.year }} -o /var/data", 
-    dag=dag
-)
+rocket_list = ["all", "falcon1", "falcon9", "falconheavy"]
 
-t2 = BashOperator(
-    task_id="print_data", 
-    bash_command="cat /var/data/year={{ execution_date.year }}/rocket={{ params.rocket }}/data.csv", 
-    params={"rocket": "all"}, # falcon1/falcon9/falconheavy
-    dag=dag
-)
+for rocket in rocket_list:
+    t1 = BashOperator(
+        task_id="get_data_"+rocket, 
+        bash_command="python3 /root/airflow/dags/spacex/load_launches.py -y {{ execution_date.year }} -o /var/data %s" % "" if rocket == "all" else " -r "+rocket, 
+        dag=dag
+    )
 
-t1 >> t2
+    t2 = BashOperator(
+        task_id="print_data_"+rocket, 
+        bash_command="cat /var/data/year={{ execution_date.year }}/rocket=%s/data.csv" % rocket, 
+        dag=dag
+    )
+
+    t1 >> t2
